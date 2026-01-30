1a2f4c6869c0918f3716223bf7963d724e1bfc50
egeevis <egeevis@gmail.com>
2026-01-30 13:08:12 +0300

    build code

--- BookStore.Api/Program.cs
+++ BookStore.Api/Program.cs
@@ -41,9 +41,6 @@ builder.Services.AddDbContext<BookStoreContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
-var app = builder.Build();
-
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
